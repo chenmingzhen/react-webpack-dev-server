638e0c26e760c928912de0fc67a99505dc3c790d
chenmingzhen <214164051@qq.com>
2020-11-17 13:22:18 +0800

    fix sockjs bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>111222</div>, document.getElementById("root"));
+ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 //const got = require("got");
-const request=require("request")
+const request = require("request")
 const Koa = require("koa");
 const Router = require("koa-router");
 const webpack = require("webpack");
@@ -7,62 +7,69 @@ const WebpackDevServer = require("webpack-dev-server");
 const ejs = require("ejs");
 
 const webpackConfig = {
-  entry: {
-    app: "./index.js",
-  },
-  resolve: {
-    extensions: [".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/, //这个文件夹除外
-        loader: "babel-loader",
-      },
-    ],
-  },
-  output: {
-    filename: "[name].js",
-    publicPath: `http://localhost:3000/`,
-  },
+    entry: {
+        app: "./index.js",
+    },
+    resolve: {
+        extensions: [".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, //这个文件夹除外
+                loader: "babel-loader",
+            },
+        ],
+    },
+    output: {
+        filename: "[name].js",
+        publicPath: `http://localhost:3000/`,
+    },
 };
 
-new WebpackDevServer(webpack(webpackConfig),{hot:false}).listen(
-  3001,
-  "localhost",
-  (err) => {
-    console.log(err);
-  }
+new WebpackDevServer(webpack(webpackConfig), {
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
+    host:"0.0.0.0",
+    port:3001,
+    public:"0.0.0.0:3001"
+}).listen(
+    3001,
+    "0.0.0.0",
+    (err) => {
+        console.log(err);
+    }
 );
 
 const app = new Koa();
 const router = new Router();
 
 router.get("/*.*", async (ctx, next) => {
-  let url = ctx.url.split("/");
-  url = url[url.length - 1];
-  const options = {
-    url: `http://localhost:3001/${url}`,
-    method: "GET",
-  };
-  console.log(ctx.url, options);
-  /* await got(options.url).then((data) => {
-    console.log(data);
-    ctx.body = data.body;
-  }); */
-  ctx.body=request(options)
+    let url = ctx.url.split("/");
+    url = url[url.length - 1];
+    const options = {
+        url: `http://localhost:3001/${url}`,
+        method: "GET",
+    };
+    console.log(ctx.url, options);
+    /* await got(options.url).then((data) => {
+      console.log(data);
+      ctx.body = data.body;
+    }); */
+    ctx.body = request(options)
 });
 
 router.get("/*", async (ctx) => {
-  ctx.set("Access-Control-Allow-Origin", "*");
-  ctx.body = await ejs.renderFile(`./index.html`, {
-    scripts: ["app.js"],
-  });
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.body = await ejs.renderFile(`./index.html`, {
+        scripts: ["app.js"],
+    });
 });
 
 app.use(router.routes());
 
 app.listen(3000, () => {
-  console.log(`server running on http://localhost:3000`);
+    console.log(`server running on http://localhost:3000`);
 });
